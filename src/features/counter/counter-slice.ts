712db92d930d63f1375a32f6113f2512d4a38a61
Ruslan <ruslan.gcnto@yandex.kz>
2024-07-10 23:14:14 +0500

    add simple counter

--- src/features/counter/counter-slice.ts
+++ src/features/counter/counter-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CounterState {
   value: number
@@ -7,3 +7,18 @@ interface CounterState {
 const initialState: CounterState = {
   value: 0,
 }
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    incremented(state) {
+      // It's ok to do this, immer js makes it immutable
+      state.value++;
+    }
+  }
+})
+
+export const { incremented } = counterSlice.actions;
+
+export default counterSlice.reducer;
